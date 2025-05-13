8154bf8b9966d37b8852a942d4c67fd04f1e0e1e
Adelphos21 <esquelat@gmail.com>
2025-05-13 17:39:59 -0500

    @Component

---
+++ src/main/java/com/tuapp/listener/EmailNotificationListener.java
@@ -0,0 +1,18 @@
+package com.tuapp.listener;
+
+import com.tuapp.event.OrderCreatedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailNotificationListener {
+
+    private static final Logger logger = LogManager.getLogger(EmailNotificationListener.class);
+
+    @EventListener
+    public void handleOrderCreated(OrderCreatedEvent event) {
+        logger.info("📧 Enviando correo de confirmación a: {}", event.getEmail());
+    }
+}
