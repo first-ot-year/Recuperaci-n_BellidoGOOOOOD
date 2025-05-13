b2a9d039ff7c5580e0f7ecc1d93e98e2859e99c8
first-ot-year <carlos.angel@utec.edu.pe>
2025-05-13 18:01:12 -0500

    fin

--- src/main/java/com/tuapp/listener/AuditLogListener.java
+++ src/main/java/com/tuapp/listener/AuditLogListener.java
@@ -1,4 +1,19 @@
 package com.tuapp.listener;
 
+import com.tuapp.event.OrderCreatedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuditLogListener {
+
+    private static final Logger logger = LogManager.getLogger(AuditLogListener.class);
+
+    @EventListener
+    public void handleOrderCreated(OrderCreatedEvent event) {
+        logger.info("🧾 Pedido registrado: ID={}, Email={}, Productos={}",
+                event.getOrderId(), event.getEmail(), event.getProducts().size());
+    }
 }
