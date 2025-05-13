721e6dec8afe27642b6ac1eb174c646b40be4d9e
first-ot-year <carlos.angel@utec.edu.pe>
2025-05-13 17:24:50 -0500

    creacion

---
+++ src/main/java/com/tuapp/SpringbootOrderEventSystemApplication.java
@@ -0,0 +1,11 @@
+package com.tuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOrderEventSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOrderEventSystemApplication.class, args);
+    }
+}
