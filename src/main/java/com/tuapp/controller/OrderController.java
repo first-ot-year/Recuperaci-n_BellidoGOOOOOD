b6edf6203354b21e21132d1cc60babf4d8d5113b
ana17hy <ana.huapaya@utec.edu.pe>
2025-05-13 17:33:37 -0500

    Controller y event

---
+++ src/main/java/com/tuapp/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.tuapp.controller;
+
+import com.tuapp.event.OrderCreatedEvent;
+import com.tuapp.model.OrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public String createOrder(@RequestBody OrderRequest request) {
+        Long orderId = System.currentTimeMillis();
+        publisher.publishEvent(new OrderCreatedEvent(this, orderId, request.getEmail(), request.getProducts()));
+        return "Orden recibida. ID: " + orderId;
+    }
+}
