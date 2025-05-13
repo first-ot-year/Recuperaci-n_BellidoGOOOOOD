0870a2bd049e8e23b4a60846f52acb16ecee4a47
first-ot-year <carlos.angel@utec.edu.pe>
2025-05-13 17:51:14 -0500

    fin

--- src/main/java/com/tuapp/controller/OrderController.java
+++ src/main/java/com/tuapp/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.tuapp.controller;
 
+import com.tuapp.event.OrderCreatedEvent;
 import com.tuapp.model.OrderRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
